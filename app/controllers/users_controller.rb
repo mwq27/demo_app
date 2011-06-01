389ddee607f05a55f8e8036cfb0ab11b4c8a9c79
Marques Woodson <marques.woodson@gmail.com>
2011-06-01 14:26:08 -0500

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,7 @@ before_filter :admin_user,    :only => :destroy
     @title = "All Users"
     @users = User.paginate(:page => params[:page])
   end
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
-  end
-  
+    
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -45,16 +41,26 @@ before_filter :admin_user,    :only => :destroy
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User desctroyed"
+    @user = User.find(params[:id])  
+      if current_user.admin? && @user != current_user
+        @user.destroy 
+    flash[:success] = "User destroyed"
     redirect_to users_path
+    else
+      flash[:notice]= "Can not delete yourself"
+      redirect_to(user_path)
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
   end
   
   
   private 
-    def authenticate
-      deny_access unless signed_in?
-    end
+   
     
     def correct_user
       @user = User.find(params[:id])
